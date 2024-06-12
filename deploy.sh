2c358c489fab78652841b37cb76d4614b4cf0ec8
Soundarya3769 <158430218+Soundarya3769@users.noreply.github.com>
2024-06-12 09:45:43 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum install python3-pip -y
+git clone https://github.com/soundarya3769/USA-Housing.git
+cd USA-Housing
+pip3 install -r requirements.txt
+screen -m -d python3 app.py 
